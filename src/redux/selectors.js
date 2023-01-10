a5fa48c9469dcf702e5609ec4895cf290531b144
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2023-01-10 17:32:05 +0700

    da xoa code cu, nhin clear code

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,14 +1,3 @@
-// // export const todoListSelector = (state) => state.todoList;
-// export const todoListSelector = (state) => {
-//     const todoShowResults = state.todoList.filter((data) => {
-//         return data.name.includes(state.filters.search);
-//     })
-//     return todoShowResults;
-// }
-
-// export const searchTextSelector = (state) => state.filters.search;
-
-// ----------------------sử dụng reselect cho gọn code
 import { createSelector } from "reselect";
 
 export const todoListSelector = (state) => state.todoList;
